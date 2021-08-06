3ac63f9ea052fffa498dd640f5360b680fbe29d3
LucaMH <luca.hollaender@googlemail.com>
2021-08-06 19:51:38 +0200

    Update LUNIX-update_os_and_software.sh
    added base structure

--- linux/LUNIX-update_os_and_software.sh
+++ linux/LUNIX-update_os_and_software.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
+##HEADER
 #RELEASE         : 20210906
 #CREATOR         : LucaMH
 #CONTRIBUTORS    : -
 #LICENSE         : MIT
 #DISCLAIMER      : if this script breaks your system, it is your fault you ran conde before checking the code, always check code which you dont own before running it!
 
+##GLOBAL SETTINGS
+#Variables
+
+##MAIN
+#RUNNING UPDATES
+UPDATE_NONOS
+UPDATE_OS
+
+#reboot
+echo "script finished"
+echo "rebooting now"
+reboot now
+systemctl reboot now
+shutdown -r now
+
+exit
+
+##FUNCTIONS
+#update appliances with this function
+UPDATE_NONOS () {
+
+}
+
+UPDATE_OS () {
+
+}
