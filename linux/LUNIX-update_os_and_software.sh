0306e1c9c4217005634c7b582a1fa493a7a7e97e
LucaMH <luca.hollaender@googlemail.com>
2021-08-06 21:40:59 +0200

    Update LUNIX-update_os_and_software.sh
    added PIP2 and PIP3 in NONOS section
    
    Co-Authored-By: Sagamir <Sagamir@users.noreply.github.com>

--- linux/LUNIX-update_os_and_software.sh
+++ linux/LUNIX-update_os_and_software.sh
@@ -7,7 +7,7 @@
 #DISCLAIMER      : if this script breaks your system, it is your fault you ran conde before checking the code, always check code which you dont own before running it!
 
 ##GLOBAL SETTINGS
-#Variables
+#VARIABLES
 
 ##MAIN
 #RUNNING UPDATES
@@ -26,6 +26,35 @@ exit
 ##FUNCTIONS
 #update appliances with this function
 UPDATE_NONOS () {
+   
+    #update pip2 if existing (Thanks to @Sagamir)
+    echo "trying to update pip2"
+    if [ -x "$(command -v pip2)" ]
+    then
+        echo "updating pip2"
+        pip2 install --upgrade pip
+        sleep 2s
+        echo "updating outdated pip2 packages"
+        pip2 list --outdated | cut -f1 -d' ' | awk '{if(NR>=3)print}' | xargs -n1 pip install --upgrade
+        sleep 2s
+        echo "done"
+    else
+        echo "there is no pip2"
+    fi 
+
+    #update pip3 if existing (Thanks to @Sagamir)
+    echo "trying to update pip3"
+    if [ -x "$(command -v pip3)" ]
+    then
+        echo "updating pip3"
+        pip3 install --upgrade pip
+        sleep 2s
+        echo "updating outdated pip3 packages"
+        pip3 list --outdated | cut -f1 -d' ' | tr " " "\n" | awk '{if(NR>=3)print}' | cut -d' ' -f1 | xargs -n1 pip install --upgrade
+        sleep 2s
+    else
+        echo "there is no pip3"
+    fi
 
 }
 
