4da5a5a957c2c76f17b2261e0323e285792da866
LucaMH <luca.hollaender@googlemail.com>
2021-08-06 19:44:10 +0200

    Update LUNIX-update_os_and_software.sh
    added header

--- linux/LUNIX-update_os_and_software.sh
+++ linux/LUNIX-update_os_and_software.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+#RELEASE         : 20210906
+#CREATOR         : LucaMH
+#CONTRIBUTORS    : -
+#LICENSE         : MIT
+#DISCLAIMER      : if this script breaks your system, it is your fault you ran conde before checking the code, always check code which you dont own before running it!
 
